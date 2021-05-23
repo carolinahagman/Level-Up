f745b7f9fb4b416a265878487ecf95d0b4bd1282
Fvrom <from.ida@hotmail.com>
2021-05-23 12:09:44 +0200

    added local multiplayer.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,3 @@ import { Game } from 'phaser';
 import { io, openSocket } from 'socket.io-client';
 //import socketIOClient from 'socket.io-client';
 const game = new Game(config);
-
-/** Socket **/
-//const socketIO = socketIOClient.connect(SERVER_URL);
-const SERVER_URL = 'http://localhost:8080';
-const socket = io.connect(SERVER_URL);
-
-//const socket = require('socket.io-client')(SERVER_URL);
-/*   {
-  rejectUnauthorized: false, // WARN: please do not do this in production
-});
- */
-//const socket = io('http://localhost:8080');
-/* 
-socket.on('connect_error', (err) => {
-  console.log(`connect_error due to ${err.message}`);
-});*/
--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -1,6 +1,13 @@
 import Phaser, { Scene } from 'phaser';
 import Background from '../assets/BG.png';
 
+let P1Display;
+let P2Display;
+let P1Ready = false;
+let P2Ready = false;
+
+let cursors;
+
 class StartScene extends Scene {
   constructor() {
     super('preload');
@@ -9,9 +16,8 @@ class StartScene extends Scene {
   preload() {
     this.load.image('background', Background);
   }
-  create() {
-    // this.socket = io();
 
+  create() {
     this.add.image(
       this.game.config.width / 2,
       this.game.config.height / 2,
@@ -19,8 +25,8 @@ class StartScene extends Scene {
     );
 
     this.TitleText = this.add.text(
-      720,
-      50,
+      700,
+      120,
       'Towerfall',
 
       {
@@ -29,21 +35,57 @@ class StartScene extends Scene {
       }
     );
 
-    this.StartText = this.add.text(
-      720,
-      100,
-      'Start',
+    P1Display = this.add.text(400, 700, '', {
+      font: '20px Courier',
+      fill: '#00ff00',
+    });
 
-      {
-        fontSize: '40px',
-        fill: '#000',
-      }
-    );
-    this.StartText.setInteractive({ useHandCursor: true }).on(
-      'pointerdown',
-      () => this.scene.start('Game')
-    );
+    P1Display.setText(['P1 press SPACE when ready']);
+
+    P2Display = this.add.text(900, 700, '', {
+      font: '20px Courier',
+      fill: '#00ff00',
+    });
+    P2Display.setText(['P2 press UP when ready']);
+
+    this.anims.create({
+      key: 'space',
+    });
+    this.anims.create({
+      key: 'up',
+    });
+
+    // keys = this.input.keyboard.addKeys('UP, SPACE');
+    cursors = this.input.keyboard.createCursorKeys();
   }
-  update() {}
+
+  update() {
+    if (cursors.space.isDown) {
+      P1Ready = true;
+      P1Display.setText(['Player 1 is ready']);
+    }
+    if (cursors.up.isDown) {
+      P2Ready = true;
+      P2Display.setText(['Player 2 is ready']);
+    }
+
+    if (P2Ready === true && P1Ready === true) {
+      this.StartText = this.add.text(
+        720,
+        250,
+        'Start',
+
+        {
+          fontSize: '60px',
+          fill: '#000',
+        }
+      );
+      this.StartText.setInteractive({ useHandCursor: true }).on(
+        'pointerdown',
+        () => this.scene.start('Game')
+      );
+    }
+  }
 }
+
 export default StartScene;
