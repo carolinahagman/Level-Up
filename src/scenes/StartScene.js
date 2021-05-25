10a441d4599825e0a28b36501580643ae93eea3b
Fvrom <from.ida@hotmail.com>
2021-05-25 09:51:08 +0200

    added controls guide

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -6,6 +6,9 @@ let p2Display;
 let p1Ready = false;
 let p2Ready = false;
 
+let p1controlsText;
+let p2controlsText;
+
 let cursors;
 
 class StartScene extends Scene {
@@ -24,14 +27,27 @@ class StartScene extends Scene {
       'background'
     );
 
-    this.titleText = this.add.text(
-      700,
-      120,
-      'Towerfall',
+    this.titleText = this.add.text(720, 120, 'Towerfall', {
+      font: '40px Georgia',
+      fill: '#000',
+    });
 
+    p1controlsText = this.add.text(
+      200,
+      250,
+      ' CONTROLS\n Player 1\n\n A - Go left\n D - Go right\n W - Jump\n SPACE - Shoot',
       {
-        fontSize: '40px',
-        fill: '#000',
+        font: '25px Courier',
+        fill: '#f1f1f1',
+      }
+    );
+    p2controlsText = this.add.text(
+      1190,
+      250,
+      ' CONTROLS\n Player 2\n\n \u2190 - Go left\n \u2192 - Go right\n \u2191 - Jump\n ENTER - Shoot',
+      {
+        font: '25px Courier',
+        fill: '#f1f1f1',
       }
     );
 
@@ -41,12 +57,28 @@ class StartScene extends Scene {
     });
 
     p1Display.setText(['P1 press SPACE when ready']);
+    this.tweens.add({
+      targets: p1Display,
+      alpha: 0,
+      ease: 'easeIn',
+      duration: 1300,
+      repeat: -1,
+      yoyo: true,
+    });
 
     p2Display = this.add.text(900, 700, '', {
       font: '20px Courier',
       fill: '#00ff00',
     });
     p2Display.setText(['P2 press UP when ready']);
+    this.tweens.add({
+      targets: p2Display,
+      alpha: 0,
+      ease: 'easeIn',
+      duration: 1300,
+      repeat: -1,
+      yoyo: true,
+    });
 
     this.anims.create({
       key: 'space',
@@ -69,14 +101,32 @@ class StartScene extends Scene {
       p2Display.setText(['Player 2 is ready']);
     }
 
+    if (p1Ready === true) {
+      this.tweens.add({
+        targets: p1Display,
+        alpha: 1,
+        ease: 'easeIn',
+        repeat: 0,
+      });
+    }
+
+    if (p2Ready === true) {
+      this.tweens.add({
+        targets: p2Display,
+        alpha: 1,
+        ease: 'easeIn',
+        repeat: 0,
+      });
+    }
+
     if (p2Ready === true && p1Ready === true) {
       this.startText = this.add.text(
-        720,
-        250,
+        750,
+        260,
         'Start',
 
         {
-          fontSize: '60px',
+          fontSize: '50px Georgia',
           fill: '#000',
         }
       );
